298a2789f5e32638f743ba80df05e24325ebc219
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-06-24 15:14:40 +0530

    Canvas component created

--- src/routes/ThreePaintRoutes.tsx
+++ src/routes/ThreePaintRoutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CanvasComponent from '../components/CanvasComponent/CanvasComponent';
 
 export default function ThreePaintRoutes() {
   return (
     <Router>
+      <CanvasComponent />
       <Routes>
-        
+
       </Routes>
     </Router>
   )
